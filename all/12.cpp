6e2bacb4f99e5267a8913c9ce0ed2dba87e73b62
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-08 15:37:09 +0800

    Update 12.cpp

--- all/12.cpp
+++ all/12.cpp
@@ -1,3 +1,4 @@
+//中國餘數定理
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -8,17 +9,14 @@ int main() {
     ifstream f("e.txt");
     vector<int> p, r;
     int v1, v2;
-
     while (f >> v1 >> v2) {
         p.push_back(v1); r.push_back(v2);
         cout << v1 << " " << v2 << " ";
     }
     cout << endl;
     if (p.empty()) return 0;
-
     long long N = 1, X = 0;
     for (int i = 0; i < (int)p.size(); i++) N *= p[i];
-
     for (int i = 0; i < (int)p.size(); i++) {
         long long Mi = N / p[i];
         int yi = 1;
@@ -26,7 +24,6 @@ int main() {
         
         X += Mi * yi * r[i];
     }
-
     cout << "Ans=" << X % N << endl;
     return 0;
 }
