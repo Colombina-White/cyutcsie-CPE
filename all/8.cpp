d9f17712932e15c19de8dbdefd871c3710bf6dfe
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-08 15:36:21 +0800

    Update 8.cpp

--- all/8.cpp
+++ all/8.cpp
@@ -1,3 +1,7 @@
+/*假設有一個英文小寫(‘a’~’z’)的字串S，長度最多為100，小明設計了一個編碼方式，
+就是把連續相同字元的數量N串接在該字元的前面，並且N會以小寫的16進位來表示，
+底下是一個實際編碼的例子: 
+假設S=“cccccyyyyyyyyyyyyuttt” */
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,11 +13,8 @@ int main() {
     string s;
     if (!getline(file, s)) return 0;
     file.close();
-
     cout << s << "\nAns=";
-
     cout << hex; 
-
     int cnt = 1;
     for (int i = 0; i < (int)s.length(); i++) {
         if (i + 1 < (int)s.length() && s[i] == s[i+1]) {
@@ -24,6 +25,5 @@ int main() {
         }
     }
     cout << endl;
-
     return 0;
 }
