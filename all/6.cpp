4a448ddfa70aac0b185435a67fd6aaf3ffad4081
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-08 15:35:50 +0800

    Update 6.cpp

--- all/6.cpp
+++ all/6.cpp
@@ -1,3 +1,4 @@
+//連續整數和為最大值
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -9,21 +10,17 @@ int main() {
     ifstream file("e.txt");
     vector<int> nums;
     int val;
-
     while (file >> val) nums.push_back(val);
     if (nums.empty()) return 0;
-
     int n = nums.size();
     for (int i = 0; i < n; i++)
         cout << nums[i] << (i == n - 1 ? "" : " ");
     cout << endl;
-
     long long maxSum = nums[0], curSum = nums[0];
     for (int i = 1; i < n; i++) {
         curSum = max((long long)nums[i], curSum + nums[i]);
         maxSum = max(maxSum, curSum);
     }
-
     cout << "Ans=" << maxSum << endl;
     return 0;
 }
