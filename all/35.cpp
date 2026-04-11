db1625ac313830b056f088c761ecb9862ba5ae2a
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-12 01:15:43 +0800

    Remove unnecessary lines and format code

--- all/35.cpp
+++ all/35.cpp
@@ -7,20 +7,16 @@ int main() {
     ifstream f("e.txt");
     int n, next, step = 0, history[1000] = {0}, pos[1000] = {0};
     if (!(f >> n)) return 0;
-    
     cout << n << endl;
     while (true) {
         next = 0;
         for (int t = n; t > 0; t /= 10) next += (t % 10) * (t % 10);
-        
         if (next == 1) { cout << "Ans=1" << endl; break; }
-        
-        if (pos[next]) { // 如果這個數字的「位置」已經被標記過
+        if (pos[next]) {
             cout << "Ans=" << step - pos[next] + 1 << endl;
             break;
         }
-        
-        pos[next] = ++step; // 標記這個數字是在第幾步出現的
+        pos[next] = ++step;
         n = next;
     }
     return 0;
