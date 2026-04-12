82a1b0e8a1cde3b3ef654a181766a3a5ae92ae09
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-12 15:56:18 +0800

    Update 3.cpp

--- all/3.cpp
+++ all/3.cpp
@@ -24,3 +24,38 @@ int main() {
     cout << "Ans=" << s << endl;
     return 0;
 }
+//不用函示
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream f("3.txt");
+    char s[1000];
+    if (!(f.getline(s, 1000))) return 0;
+    cout << s << endl;
+
+    int n = 0;
+    while (s[n]) n++;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] == 'A' || s[i] == 'a') {
+            int j = i;
+            while (s[j] && s[j] != 'L' && s[j] != 'l') j++;
+
+            if (s[j]) {
+                int k = 0;
+                while (s[j + 1 + k]) {
+                    s[i + k] = s[j + 1 + k];
+                    k++;
+                }
+                s[i + k] = '\0';
+                i = i; 
+            }
+        }
+    }
+
+    cout << "Ans=" << s << endl;
+    return 0;
+}
