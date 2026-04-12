80d6766897f6e7407e7f2616eeea8ff2bc2112ff
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-12 15:36:41 +0800

    Update 2.cpp

--- all/2.cpp
+++ all/2.cpp
@@ -32,3 +32,35 @@ int main() {
     cout << temp << "\nAns=" << res << endl;
     return 0;
 }
+//不用函示
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void factor(long long n, int i) {
+    if (n == 1) return;
+    while (i * i <= n && n % i != 0) i++;
+    if (n % i == 0) {
+        int cnt = 0;
+        long long next_n = n;
+        while (next_n % i == 0) {
+            cnt++;
+            next_n /= i;
+        }
+        factor(next_n, i + 1);
+        cout << i << "(" << cnt << ")";
+    } else {
+        cout << n << "(1)";
+    }
+}
+
+int main() {
+    ifstream f("a.txt");
+    long long n;
+    if (!(f >> n)) return 0;
+    cout << n << "\nAns=";
+    factor(n, 2);
+    cout << endl;
+    return 0;
+}
