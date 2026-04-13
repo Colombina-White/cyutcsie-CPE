c6d81856f65a9856bb0998d69635bcfda085e848
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-13 14:28:07 +0800

    Update 4.cpp

--- all/4.cpp
+++ all/4.cpp
@@ -1,3 +1,8 @@
+/*讀取第一個數字：先讀入 res 作為運算的起點。
+迴圈讀取配對：每次讀取一個 op（運算子）和一個 next（下一個數字）。
+執行運算：根據 op 的種類，將 res 與 next 進行計算。
+終止條件：讀到 = 符號時停止運算。*/
+
 //算術運算式
 #include <iostream>
 #include <fstream>
@@ -28,6 +33,9 @@ int main() {
     return 0;
 }
 //不用函示
+/*f >> res：這會跳過空白，讀取第一個浮點數。
+f >> op：讀取一個字元（運算子）。
+f >> next：緊接著讀取下一個浮點數。*/
 #include <iostream>
 #include <fstream>
 
