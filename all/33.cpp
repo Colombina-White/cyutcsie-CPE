329a2f44cee3f75963ce71cd73f76195bb101c10
White.Moon <117890040+Colombina-White@users.noreply.github.com>
2026-04-20 14:00:19 +0800

    Update 33.cpp

--- all/33.cpp
+++ all/33.cpp
@@ -32,3 +32,37 @@ int main() {
     cout << "Ans=" << s << endl;
     return 0;
 }
+//不用函示
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    ifstream f("33.txt");
+    string s;
+    if (!getline(f, s)) return 0;
+
+    cout << s << endl;
+
+    long long sum = 0;
+    for (int i = 0; i < (int)s.length(); i++) {
+        if (isalpha(s[i])) {
+            sum += (toupper(s[i]) - 'A' + 1);
+        }
+    }
+    int shift = (int)(sum % 26);
+
+    for (int i = 0; i < (int)s.length(); i++) {
+        if (islower(s[i])) {
+            s[i] = (s[i] - 'a' + shift) % 26 + 'a';
+        } else if (isupper(s[i])) {
+            s[i] = (s[i] - 'A' + shift) % 26 + 'A';
+        }
+    }
+
+    cout << "Ans=" << s << endl;
+    return 0;
+}
